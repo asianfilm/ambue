6759727e2d362bb0da95b7a5f8d61758fbeb8e4e
Stephen Cremin <stephen.cremin@nyaff.org>
2020-10-07 12:03:27 +0800

    chore: move form into its own live component

--- lib/ambue_web/live/page_live.ex
+++ lib/ambue_web/live/page_live.ex
@@ -1,44 +1,15 @@
 defmodule AmbueWeb.PageLive do
   use AmbueWeb, :live_view
 
-  alias Ambue.Accounts
-  alias Ambue.Accounts.User
+  alias AmbueWeb.FormComponent
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok,
-     assign(
-       socket,
-       changeset: Accounts.change_user(%User{})
-     )}
-  end
+  def mount(_params, _session, socket), do: {:ok, socket}
 
   @impl true
-  def handle_event("signup", %{"user" => params}, socket) do
-    case Accounts.create_user(params) do
-      {:ok, _} ->
-        {:noreply,
-         assign(socket,
-           changeset: Accounts.change_user(%User{})
-         )}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply,
-         assign(socket,
-           changeset: changeset
-         )}
-    end
-  end
-
-  @impl true
-  def handle_event("validate", %{"user" => params}, socket) do
-    {:noreply,
-     assign(socket,
-       changeset:
-         %User{}
-         |> Accounts.change_user(params)
-         |> IO.inspect()
-         |> Map.put(:action, :insert)
-     )}
-  end
+  def render(assigns),
+    do: ~L"""
+    <h1>Sign Up!</h1>
+    <%= live_component @socket, FormComponent, id: 1 %>
+    """
 end
