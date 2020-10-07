64191cbe24866d81e33c8d2ca562ea0d7a24279c
Stephen Cremin <stephen.cremin@nyaff.org>
2020-10-07 15:33:11 +0800

    chore: add credo static code analysis tool

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule Ambue.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.2.2", only: [:dev, :test], runtime: false}
     ]
   end
 
