480a985b29cc81af68de234cef98306425cb1097
Stephen Cremin <stephen.cremin@nyaff.org>
2020-10-07 10:54:00 +0800

    feature: add changeset validations; testing in iex

--- lib/ambue/accounts/user.ex
+++ lib/ambue/accounts/user.ex
@@ -2,6 +2,8 @@ defmodule Ambue.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @email ~r/^[a-z0-9._%+-+']+@[a-z0-9.-]+\.[a-z]+$/i
+
   schema "users" do
     field :email, :string
     field :name, :string
@@ -14,5 +16,9 @@ defmodule Ambue.Accounts.User do
     user
     |> cast(attrs, [:name, :email])
     |> validate_required([:name, :email])
+    |> unique_constraint(:email, name: "users_email_index", message: "Email already in use")
+    |> unique_constraint(:name, name: "users_name_index", message: "Name already in use")
+    |> validate_length(:name, min: 3, max: 30)
+    |> validate_format(:email, @email, message: "Please enter a valid email!")
   end
 end
